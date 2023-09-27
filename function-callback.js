a97e85ceef6aff373586e6b9b0f4b886afca27e0
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-09-27 15:34:11 +0900

    function zero() 함수 생성하고 매개변수 채우기

--- function-callback.js
+++ function-callback.js
@@ -1,3 +1,7 @@
 // Mission 1. 매개변수 두 개 마련하고, 덧셈한 값을 숫자로 리턴하는 함수 zero()를 만들기
 // 	1-1. 선언을 다 한 다음, 호출해서 처리하는 함수 one() 만들기
 // 	1-2. 선언만 간단히 한 다음, 호출할 때, 결정하는 함수 two() 만들기
+// 1.
+function zero(one, two, callback) {
+  return callback(one, two);
+}
