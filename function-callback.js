c443b784e82ad5fa5ed92824f8966ad6ef6d95f0
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-09-27 15:33:25 +0900

    헷갈리는 것 같아서 재시작

--- function-callback.js
+++ function-callback.js
@@ -1,23 +1,3 @@
 // Mission 1. 매개변수 두 개 마련하고, 덧셈한 값을 숫자로 리턴하는 함수 zero()를 만들기
 // 	1-1. 선언을 다 한 다음, 호출해서 처리하는 함수 one() 만들기
 // 	1-2. 선언만 간단히 한 다음, 호출할 때, 결정하는 함수 two() 만들기
-
-// 1. function zero()
-function zero(firstNumber, secondNumber) {
-  if (typeof firstNumber === "number" || secondNumber === "number") {
-    return firstNumber + secondNumber;
-  }
-}
-console.log(zero(1, 2)); // 성공
-console.clear(); // 다음 콘솔 확인을 위한 클리닝
-
-// 1-1. function one()
-function one() {
-  return zero(2, 3);
-}
-console.log(one());
-
-// 1-2. function two()
-// function two(callback) {
-//   callback(){}
-// }
